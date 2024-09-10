b9554ba7e01cb88c1dd50469d48e720f4eac4c02
Ashvin Jaiswal <ashvin.edu@gmail.com>
2024-09-10 18:29:25 +0100

    Add steps logic for scenario

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,18 +1,25 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
+const Person = require('./../../src/shouty.js');
+const { assertThat, is } = require("hamjest");
 
 Given("Lucy is located {int} metres from Sean", function (distance) {
-  console.log(distance);
+  this.lucy = new Person();
+  this.sean = new Person();
+  this.lucy.moveTo(distance);
   // Given('Lucy is located {float} metres from Sean', function (float) {
   // Write code here that turns the phrase above into concrete actions
-  return "pending";
 });
 
-When("Sean shouts {string}", function (string) {
+When("Sean shouts {string}", function (message) {
+  console.log(message);
+  this.sean.shout(message);
+  this.message = message
   // Write code here that turns the phrase above into concrete actions
-  return "pending";
+  // return "pending";
 });
 
 Then("Lucy hears Sean's message", function () {
+  assertThat(this.lucy.messageHeard(), is([this.message]))
   // Write code here that turns the phrase above into concrete actions
-  return "pending";
+  // return "pending";
 });
